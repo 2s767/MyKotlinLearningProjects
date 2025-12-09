9799cac44e04adcaa3825e4f2bb669e36528b95f
Asror <yodgorovasror37@gmail.com>
2025-12-09 12:35:20 +0500

    RadioButton

--- app/src/main/java/com/example/components/MainActivity.kt
+++ app/src/main/java/com/example/components/MainActivity.kt
@@ -1,44 +1,27 @@
 package com.example.components
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Checkbox
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.components.ui.theme.ComponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,7 +32,6 @@ class MainActivity : ComponentActivity() {
             ComponentsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Components(
-
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -60,51 +42,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Components(modifier: Modifier = Modifier) {
-    val genderMale  = remember {
-        mutableStateOf(false)
-    }
-    val genderFemale  = remember {
-        mutableStateOf(false)
-    }
-    val title = remember {
-        mutableStateOf("What is your Gender ?")
-    }
-    fun genderChooser() {
-        if(genderFemale.value && genderMale.value) title.value =  "What is your gender ? " else if (genderMale.value ) title.value =  " Your Gender is Male" else title.value = "Your Gender is Female"
+    val colorsName = listOf("White","Red","Blue","Green")
+    val backGroundColors = listOf(Color.White, Color.Red, Color.Blue,Color.Green)
+    val indexNumber = remember {
+        mutableStateOf(0)
     }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize().background(color = backGroundColors[indexNumber.value]),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
     ) {
-        Spacer(Modifier.height(20.dp))
-        Text(title.value, fontSize = 25.sp, textAlign = TextAlign.Center, color = Color.Blue)
-        Column(
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Row( verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(checked = genderMale.value, onCheckedChange = {
-                    genderMale.value = it
-                    genderFemale.value = !it
-                    genderChooser()
-                })
-                Text("Male")
-            }
-            Spacer(Modifier.height(20.dp))
-            Row( verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = genderFemale.value, onCheckedChange = {
-                        genderFemale.value = it
-                        genderMale.value = !it
-                        genderChooser()
-                    })
-                Text("Female")
-            }
-        }
 
+        colorsName.forEachIndexed { position, name ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 120.dp).clickable(
+                    onClick = {
+                        indexNumber.value = position
+                    }
+                )
+            ) {
+                RadioButton(selected = name == colorsName[indexNumber.value], onClick = {
+                    indexNumber.value = position
+                },
+                    colors = RadioButtonDefaults.colors(backGroundColors[indexNumber.value])
+                    )
+                Text(colorsName[position])
+            }
+         }
     }
-
 }
 
 
